3f08868231366dc653182793e42c6e631de8c978
sathishragu <srathin@L-SNVN1RQDV7-M.local>
2015-05-30 15:49:07 -0700

    Adding API schema

---
+++ models/api.js
@@ -0,0 +1,42 @@
+let mongoose = require('mongoose')
+let bcrypt = require('bcrypt')
+let nodeify = require('bluebird-nodeify')
+
+require('songbird')
+
+let apiSchema = mongoose.Schema({
+	apiname: {
+		type: String,
+		required: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	endpoint: {
+		type: String,
+		required: true
+	},
+	enablecaching: {
+		type: Boolean,
+		required: true
+	},
+	cacheparams: {
+		type: String
+	},
+	ttl: {
+		type: Number
+	},
+	enabledebug: {
+		type: Boolean,
+		required: true
+	},
+	reqparams: {
+		type: String
+	},
+	validators: {
+		type: String
+	}
+})
+
+module.exports = mongoose.model('Api', apiSchema)
